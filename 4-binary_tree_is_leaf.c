c6ee9e47a77e423aeff1b76acb00e6ee8ad4776f
vagrant <vagrant@linux-vag20>
2023-08-29 05:59:19 +0000

    Fix task 4 betty is leaf

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -2,7 +2,10 @@
 
 
 /**
+ * binary_tree_is_leaf - checks if the given node is leaf (has no children)
+ * @node: given node to check
  *
+ * Return: 1 if its leaf else 0 if not and in faliure
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
