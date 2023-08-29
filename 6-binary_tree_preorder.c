7743c85a2ba77722ede0cff02b80d217db911a65
vagrant <vagrant@linux-vag20>
2023-08-29 06:16:04 +0000

    Add task 6 pre-order

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_preorder - preform pre-order traversal on a tree
+ * @tree: given tree
+ * @func: function to print tree values
+ *
+ * Return: Nothing
+*/
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+
+	func(tree->n);
+
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
