373f092a8f8812816868a82d3611c942b1ae1201
vagrant <vagrant@linux-vag20>
2023-08-31 15:20:28 +0000

    Add task 13 tree nodes

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_nodes - count the tree nodes
+ * @tree: point to search from
+ *
+ * Return: nodes count
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (!tree)
+		return (0);
+
+	if (tree->left || tree->right)
+		nodes += 1;
+
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+
+	return (nodes);
+}
